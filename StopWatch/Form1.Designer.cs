52e1abaeba220060ea181bb5c5f9aee9f058448f
yd-ii11 <yazeed.d.alsulami@gmail.com>
2024-04-24 10:02:54 +0300

    Yazeed Alsulami - 44004660 703

--- StopWatch/Form1.Designer.cs
+++ StopWatch/Form1.Designer.cs
@@ -34,45 +34,51 @@
             ComputerMinute = new Label();
             ComputerSecond = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            label5 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Black", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(137, 273);
+            label1.Font = new Font("Segoe UI Black", 20F);
+            label1.Location = new Point(157, 364);
             label1.Name = "label1";
-            label1.Size = new Size(228, 37);
+            label1.Size = new Size(285, 46);
             label1.TabIndex = 0;
             label1.Text = "Computer Clock";
             // 
             // ComputerHour
             // 
             ComputerHour.BorderStyle = BorderStyle.FixedSingle;
-            ComputerHour.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            ComputerHour.Location = new Point(137, 351);
+            ComputerHour.Font = new Font("Segoe UI", 20F);
+            ComputerHour.Location = new Point(157, 468);
             ComputerHour.Name = "ComputerHour";
-            ComputerHour.Size = new Size(51, 46);
+            ComputerHour.Size = new Size(58, 61);
             ComputerHour.TabIndex = 1;
             ComputerHour.Text = "00";
             // 
             // ComputerMinute
             // 
             ComputerMinute.BorderStyle = BorderStyle.FixedSingle;
-            ComputerMinute.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            ComputerMinute.Location = new Point(224, 351);
+            ComputerMinute.Font = new Font("Segoe UI", 20F);
+            ComputerMinute.Location = new Point(256, 468);
             ComputerMinute.Name = "ComputerMinute";
-            ComputerMinute.Size = new Size(51, 46);
+            ComputerMinute.Size = new Size(58, 61);
             ComputerMinute.TabIndex = 1;
             ComputerMinute.Text = "00";
             // 
             // ComputerSecond
             // 
             ComputerSecond.BorderStyle = BorderStyle.FixedSingle;
-            ComputerSecond.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            ComputerSecond.Location = new Point(314, 351);
+            ComputerSecond.Font = new Font("Segoe UI", 20F);
+            ComputerSecond.Location = new Point(359, 468);
             ComputerSecond.Name = "ComputerSecond";
-            ComputerSecond.Size = new Size(51, 46);
+            ComputerSecond.Size = new Size(58, 61);
             ComputerSecond.TabIndex = 1;
             ComputerSecond.Text = "00";
             // 
@@ -81,15 +87,80 @@
             timer1.Interval = 5000;
             timer1.Tick += timer1_Tick;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(106, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 20);
+            label2.TabIndex = 2;
+            label2.Text = "00";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(257, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(25, 20);
+            label3.TabIndex = 3;
+            label3.Text = "00";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(424, 112);
+            label4.Name = "label4";
+            label4.Size = new Size(25, 20);
+            label4.TabIndex = 4;
+            label4.Text = "00";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(238, 197);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Start/Stop";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(238, 258);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 6;
+            button2.Text = "Clear";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Headline R", 24F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label5.Location = new Point(16, 33);
+            label5.Name = "label5";
+            label5.Size = new Size(298, 41);
+            label5.TabIndex = 7;
+            label5.Text = "Stop watch";
+            label5.Click += label5_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(484, 461);
+            ClientSize = new Size(553, 615);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(ComputerSecond);
             Controls.Add(ComputerMinute);
             Controls.Add(ComputerHour);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -104,5 +175,11 @@
         private Label ComputerMinute;
         private Label ComputerSecond;
         private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Label label5;
     }
 }
