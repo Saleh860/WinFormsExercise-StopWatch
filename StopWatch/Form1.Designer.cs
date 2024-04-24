8db1af4ae5787b4f9e50775a0744fca1aaa1838d
Saleh860 <saleh_ibraheem@hotmail.com>
2024-04-24 08:51:41 +0300

    Add project files.

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            ComputerHour = new Label();
+            ComputerMinute = new Label();
+            ComputerSecond = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(137, 273);
+            label1.Name = "label1";
+            label1.Size = new Size(228, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Computer Clock";
+            // 
+            // ComputerHour
+            // 
+            ComputerHour.BorderStyle = BorderStyle.FixedSingle;
+            ComputerHour.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            ComputerHour.Location = new Point(137, 351);
+            ComputerHour.Name = "ComputerHour";
+            ComputerHour.Size = new Size(51, 46);
+            ComputerHour.TabIndex = 1;
+            ComputerHour.Text = "00";
+            // 
+            // ComputerMinute
+            // 
+            ComputerMinute.BorderStyle = BorderStyle.FixedSingle;
+            ComputerMinute.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            ComputerMinute.Location = new Point(224, 351);
+            ComputerMinute.Name = "ComputerMinute";
+            ComputerMinute.Size = new Size(51, 46);
+            ComputerMinute.TabIndex = 1;
+            ComputerMinute.Text = "00";
+            // 
+            // ComputerSecond
+            // 
+            ComputerSecond.BorderStyle = BorderStyle.FixedSingle;
+            ComputerSecond.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            ComputerSecond.Location = new Point(314, 351);
+            ComputerSecond.Name = "ComputerSecond";
+            ComputerSecond.Size = new Size(51, 46);
+            ComputerSecond.TabIndex = 1;
+            ComputerSecond.Text = "00";
+            // 
+            // timer1
+            // 
+            timer1.Interval = 5000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 461);
+            Controls.Add(ComputerSecond);
+            Controls.Add(ComputerMinute);
+            Controls.Add(ComputerHour);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label ComputerHour;
+        private Label ComputerMinute;
+        private Label ComputerSecond;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,29 @@
+namespace StopWatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateComputerTime();
+            timer1.Enabled = true;
+        }
+
+        private void UpdateComputerTime()
+        {
+            DateTime Now = DateTime.Now;
+            ComputerHour.Text = Now.Hour.ToString();
+            ComputerMinute.Text = Now.Minute.ToString();
+            ComputerSecond.Text = Now.Second.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateComputerTime();
+        }
+    }
+}
