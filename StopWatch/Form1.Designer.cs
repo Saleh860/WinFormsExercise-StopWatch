d1af17e20acf1e1c72d207a3d968857fdfe70d80
lina <lina@LAPTOP-654Q0T9J>
2024-04-25 15:57:42 +0300

    added stop watch khalid ali alsami

--- StopWatch/Form1.Designer.cs
+++ StopWatch/Form1.Designer.cs
@@ -34,6 +34,13 @@
             ComputerMinute = new Label();
             ComputerSecond = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            swS = new Label();
+            swM = new Label();
+            swH = new Label();
+            label5 = new Label();
+            timer2 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -55,6 +62,7 @@
             ComputerHour.Size = new Size(51, 46);
             ComputerHour.TabIndex = 1;
             ComputerHour.Text = "00";
+            ComputerHour.Click += ComputerHour_Click;
             // 
             // ComputerMinute
             // 
@@ -65,6 +73,7 @@
             ComputerMinute.Size = new Size(51, 46);
             ComputerMinute.TabIndex = 1;
             ComputerMinute.Text = "00";
+            ComputerMinute.Click += ComputerMinute_Click;
             // 
             // ComputerSecond
             // 
@@ -75,17 +84,94 @@
             ComputerSecond.Size = new Size(51, 46);
             ComputerSecond.TabIndex = 1;
             ComputerSecond.Text = "00";
+            ComputerSecond.Click += ComputerSecond_Click;
             // 
             // timer1
             // 
-            timer1.Interval = 5000;
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
             timer1.Tick += timer1_Tick;
+            // 
+            // swS
+            // 
+            swS.BorderStyle = BorderStyle.FixedSingle;
+            swS.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            swS.Location = new Point(305, 139);
+            swS.Name = "swS";
+            swS.Size = new Size(49, 46);
+            swS.TabIndex = 2;
+            swS.Text = "00";
+            swS.Click += swS_Click;
+            // 
+            // swM
+            // 
+            swM.BorderStyle = BorderStyle.FixedSingle;
+            swM.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            swM.Location = new Point(215, 139);
+            swM.Name = "swM";
+            swM.Size = new Size(51, 46);
+            swM.TabIndex = 3;
+            swM.Text = "00";
+            // 
+            // swH
+            // 
+            swH.BorderStyle = BorderStyle.FixedSingle;
+            swH.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            swH.Location = new Point(137, 139);
+            swH.Name = "swH";
+            swH.Size = new Size(51, 46);
+            swH.TabIndex = 4;
+            swH.Text = "00";
+            swH.Click += swH_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(215, 61);
+            label5.Name = "label5";
+            label5.Size = new Size(139, 37);
+            label5.TabIndex = 5;
+            label5.Text = "stopwatch";
+            label5.Click += label5_Click;
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+            timer2.Tick += timer2_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(215, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "clear";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(296, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 7;
+            button2.Text = "start";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(484, 461);
+            ClientSize = new Size(549, 416);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(swS);
+            Controls.Add(swM);
+            Controls.Add(swH);
             Controls.Add(ComputerSecond);
             Controls.Add(ComputerMinute);
             Controls.Add(ComputerHour);
@@ -104,5 +190,12 @@
         private Label ComputerMinute;
         private Label ComputerSecond;
         private System.Windows.Forms.Timer timer1;
+        private Label swS;
+        private Label swM;
+        private Label swH;
+        private Label label5;
+        private System.Windows.Forms.Timer timer2;
+        private Button button1;
+        private Button button2;
     }
 }
--- StopWatch/Form1.cs
+++ StopWatch/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace StopWatch
 {
     public partial class Form1 : Form
@@ -10,7 +12,6 @@ namespace StopWatch
         private void Form1_Load(object sender, EventArgs e)
         {
             UpdateComputerTime();
-            timer1.Enabled = true;
         }
 
         private void UpdateComputerTime()
@@ -24,6 +25,76 @@ namespace StopWatch
         private void timer1_Tick(object sender, EventArgs e)
         {
             UpdateComputerTime();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerHour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerMinute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComputerSecond_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void swH_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+
+
+            int x = int.Parse(swS.Text) + 1;
+            swS.Text = x.ToString();
+            if (x == 60)
+            {
+                swS.Text = "0";
+                int y = int.Parse(swM.Text) + 1;
+                swM.Text = y.ToString();
+                if (y == 60)
+                {
+                    swM.Text = "0";
+                    int z = int.Parse(swH.Text) + 1;
+                    swH.Text = z.ToString();
+
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            swH.Text = "0".ToString();
+            swM.Text = "0".ToString();
+            swS.Text = "0".ToString();
+        }
+
+        private void swS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { if (timer2.Enabled)
+            timer2.Enabled= false;
+        else 
+                    {
+                timer2.Enabled = true;
+            }
+
         }
     }
 }
